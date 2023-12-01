813a154abfd6b2335225755c28eed8acb8fd3e38
applepy <edh427@naver.com>
2023-12-01 10:43:21 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,9 @@ import pandas as pd
 @st.cache
 def load_data():
     # 데이터프레임을 여기에서 로드합니다
-    df = pd.read_csv('test02.csv')
+    df = pd.read_csv('result_final.csv')
     return df
- 
+
+st.header("jungtae homework")
 df = load_data()
 st.dataframe(df)
