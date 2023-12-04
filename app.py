a593b17df0c94b38b3ee44387a199bc881134414
applepy <edh427@naver.com>
2023-12-04 12:05:18 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib
-matplotlib.use('Agg')
+
 st.header("Visit Counting")
 
 @st.cache_data
